27a64450c8bf49d618cf8436e63592c59bd8a980
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-06-05 14:05:42 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,82 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
-import "@biconomy-sdk-dev/web3-auth/dist/src/style.css";
-import SocialLogin from "@biconomy/web3-auth";
-import { ChainId } from "@biconomy/core-types";
-import { ethers } from "ethers";
-import SmartAccount from "@biconomy/smart-account";
+import Social from "./components/Social.jsx";
 
-const MyComponent = () => {
-  const [smartAccount, setSmartAccount] = useState(null);
-  const [interval, enableInterval] = useState(false);
-  const sdkRef = useRef(null);
-  const [loading, setLoading] = useState(false);
-  const [provider, setProvider] = useState(null);
+function App() {
+  return (
+    <div>
+      <Social />
+    </div>
+  );
+}
 
-  useEffect(() => {
-    let configureLogin;
-    if (interval) {
-      configureLogin = setInterval(() => {
-        if (sdkRef.current && sdkRef.current.provider) {
-          setupSmartAccount();
-          clearInterval(configureLogin);
-        }
-      }, 1000);
-    }
-  }, [interval]);
-
-  async function login() {
-    if (!sdkRef.current) {
-      const socialLoginSDK = new SocialLogin();
-      await socialLoginSDK.init();
-      sdkRef.current = socialLoginSDK;
-    }
-    if (!sdkRef.current.provider) {
-      sdkRef.current.showWallet();
-      enableInterval(true);
-    } else {
-      setupSmartAccount();
-    }
-  }
-
-  async function setupSmartAccount() {
-    if (!sdkRef.current || !sdkRef.current.provider) return;
-    sdkRef.current.hideWallet();
-    setLoading(true);
-    const web3Provider = new ethers.providers.Web3Provider(
-      sdkRef.current.provider
-    );
-    setProvider(web3Provider);
-    try {
-      const smartAccount = new SmartAccount(web3Provider, {
-        activeNetworkId: ChainId.POLYGON_MUMBAI,
-        supportedNetworksIds: [ChainId.POLYGON_MUMBAI],
-        networkConfig: [
-          {
-            chainId: ChainId.POLYGON_MUMBAI,
-            dappAPIKey: "your dapp api key from biconomy dashboard",
-          },
-        ],
-      });
-      await smartAccount.init();
-      setSmartAccount(smartAccount);
-      setLoading(false);
-    } catch (err) {
-      console.log("error setting up smart account... ", err);
-    }
-  }
-
-  const logout = async () => {
-    if (!sdkRef.current) {
-      console.error("Web3Modal not initialized.");
-      return;
-    }
-    await sdkRef.current.logout();
-    sdkRef.current.hideWallet();
-    setSmartAccount(null);
-    enableInterval(false);
-  };
-
-  return <div>My Component</div>;
-};
-
-export default MyComponent;
+export default App;
